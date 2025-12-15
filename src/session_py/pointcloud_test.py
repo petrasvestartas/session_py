c84e4e0e999c024e04d708c5df1d65727b40d260
pv <petasvestartas@gmail.com>
2025-12-16 00:00:55 +0100

    pointcloud

--- src/session_py/pointcloud_test.py
+++ src/session_py/pointcloud_test.py
@@ -39,6 +39,33 @@ def test_pointcloud_constructor():
     col0 = pc.get_color(0)
     norm0 = pc.get_normal(0)
 
+    # Add points, colors, normals to empty cloud
+    pc2 = PointCloud()
+    pc2.add_point(Point(1.0, 2.0, 3.0))
+    pc2.add_color(Color(128, 64, 32, 255))
+    pc2.add_normal(Vector(1.0, 0.0, 0.0))
+
+    # Set point/color/normal at index
+    pc2.set_point(0, Point(4.0, 5.0, 6.0))
+    pc2.set_color(0, Color(200, 100, 50, 255))
+    pc2.set_normal(0, Vector(0.0, 1.0, 0.0))
+
+    # Translate with Vector offset
+    pc3 = PointCloud([Point(1.0, 2.0, 3.0)])
+    offset = Vector(10.0, 20.0, 30.0)
+    pc_iadd = PointCloud([Point(1.0, 2.0, 3.0)])
+    pc_iadd += offset
+    pc_isub = PointCloud([Point(1.0, 2.0, 3.0)])
+    pc_isub -= offset
+    pc_add = pc3 + offset
+    pc_sub = pc3 - offset
+
+    # Create from flat arrays
+    coords = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+    colors_arr = [255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255]
+    normals_arr = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0]
+    pc4 = PointCloud.from_coords(coords, colors_arr, normals_arr)
+
     MINI_CHECK(pc.name == "my_pointcloud" and pc.guid != "" and point_count == 3)
     MINI_CHECK(color_count == 3 and normal_count == 3 and is_empty == False)
     MINI_CHECK("3 points" in pcstr)
@@ -47,99 +74,19 @@ def test_pointcloud_constructor():
     MINI_CHECK(TOLERANCE.is_close(pt0[0], 0.0) and TOLERANCE.is_close(pt0[1], 0.0) and TOLERANCE.is_close(pt0[2], 0.0))
     MINI_CHECK(col0[0] == 255 and col0[1] == 0 and col0[2] == 0 and col0[3] == 255)
     MINI_CHECK(TOLERANCE.is_close(norm0[0], 0.0) and TOLERANCE.is_close(norm0[1], 0.0) and TOLERANCE.is_close(norm0[2], 1.0))
-
-
-@MINI_TEST("PointCloud", "from_coords")
-def test_pointcloud_from_coords():
-    from session_py import PointCloud
-
-    # Create from flat arrays
-    coords = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]
-    colors = [255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255]
-    normals = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0]
-    pc = PointCloud.from_coords(coords, colors, normals)
-
-    point_count = len(pc)
-    color_count = pc.color_count()
-    normal_count = pc.normal_count()
-
-    pt1 = pc.get_point(1)
-    col1 = pc.get_color(1)
-    norm1 = pc.get_normal(1)
-
-    MINI_CHECK(point_count == 3 and color_count == 3 and normal_count == 3)
-    MINI_CHECK(TOLERANCE.is_close(pt1[0], 1.0) and TOLERANCE.is_close(pt1[1], 0.0) and TOLERANCE.is_close(pt1[2], 0.0))
-    MINI_CHECK(col1[0] == 0 and col1[1] == 255 and col1[2] == 0 and col1[3] == 255)
-    MINI_CHECK(TOLERANCE.is_close(norm1[0], 0.0) and TOLERANCE.is_close(norm1[1], 0.0) and TOLERANCE.is_close(norm1[2], 1.0))
-
-
-@MINI_TEST("PointCloud", "add_and_set")
-def test_pointcloud_add_and_set():
-    from session_py import PointCloud
-    from session_py import Point
-    from session_py import Vector
-    from session_py import Color
-
-    # Empty cloud
-    pc = PointCloud()
-
-    # Add points, colors, normals
-    pc.add_point(Point(1.0, 2.0, 3.0))
-    pc.add_color(Color(128, 64, 32, 255))
-    pc.add_normal(Vector(1.0, 0.0, 0.0))
-
-    point_count = len(pc)
-    color_count = pc.color_count()
-    normal_count = pc.normal_count()
-
-    # Set point/color/normal at index
-    pc.set_point(0, Point(4.0, 5.0, 6.0))
-    pc.set_color(0, Color(200, 100, 50, 255))
-    pc.set_normal(0, Vector(0.0, 1.0, 0.0))
-
-    pt0 = pc.get_point(0)
-    col0 = pc.get_color(0)
-    norm0 = pc.get_normal(0)
-
-    MINI_CHECK(point_count == 1 and color_count == 1 and normal_count == 1)
-    MINI_CHECK(TOLERANCE.is_close(pt0[0], 4.0) and TOLERANCE.is_close(pt0[1], 5.0) and TOLERANCE.is_close(pt0[2], 6.0))
-    MINI_CHECK(col0[0] == 200 and col0[1] == 100 and col0[2] == 50 and col0[3] == 255)
-    MINI_CHECK(TOLERANCE.is_close(norm0[0], 0.0) and TOLERANCE.is_close(norm0[1], 1.0) and TOLERANCE.is_close(norm0[2], 0.0))
-
-
-@MINI_TEST("PointCloud", "translate")
-def test_pointcloud_translate():
-    from session_py import PointCloud
-    from session_py import Point
-    from session_py import Vector
-
-    # Create cloud with one point
-    pc = PointCloud([Point(1.0, 2.0, 3.0)])
-    offset = Vector(10.0, 20.0, 30.0)
-
-    # In-place add
-    pc_iadd = PointCloud([Point(1.0, 2.0, 3.0)])
-    pc_iadd += offset
-
-    # In-place subtract
-    pc_isub = PointCloud([Point(1.0, 2.0, 3.0)])
-    pc_isub -= offset
-
-    # Copy add/subtract
-    pc_add = pc + offset
-    pc_sub = pc - offset
-
-    pt_iadd = pc_iadd.get_point(0)
-    pt_isub = pc_isub.get_point(0)
-    pt_add = pc_add.get_point(0)
-    pt_sub = pc_sub.get_point(0)
-    pt_orig = pc.get_point(0)
-
-    MINI_CHECK(TOLERANCE.is_close(pt_iadd[0], 11.0) and TOLERANCE.is_close(pt_iadd[1], 22.0) and TOLERANCE.is_close(pt_iadd[2], 33.0))
-    MINI_CHECK(TOLERANCE.is_close(pt_isub[0], -9.0) and TOLERANCE.is_close(pt_isub[1], -18.0) and TOLERANCE.is_close(pt_isub[2], -27.0))
-    MINI_CHECK(TOLERANCE.is_close(pt_add[0], 11.0) and TOLERANCE.is_close(pt_add[1], 22.0) and TOLERANCE.is_close(pt_add[2], 33.0))
-    MINI_CHECK(TOLERANCE.is_close(pt_sub[0], -9.0) and TOLERANCE.is_close(pt_sub[1], -18.0) and TOLERANCE.is_close(pt_sub[2], -27.0))
-    MINI_CHECK(TOLERANCE.is_close(pt_orig[0], 1.0) and TOLERANCE.is_close(pt_orig[1], 2.0) and TOLERANCE.is_close(pt_orig[2], 3.0))
+    MINI_CHECK(len(pc2) == 1 and pc2.color_count() == 1 and pc2.normal_count() == 1)
+    MINI_CHECK(TOLERANCE.is_close(pc2.get_point(0)[0], 4.0) and TOLERANCE.is_close(pc2.get_point(0)[1], 5.0) and TOLERANCE.is_close(pc2.get_point(0)[2], 6.0))
+    MINI_CHECK(pc2.get_color(0)[0] == 200 and pc2.get_color(0)[1] == 100 and pc2.get_color(0)[2] == 50 and pc2.get_color(0)[3] == 255)
+    MINI_CHECK(TOLERANCE.is_close(pc2.get_normal(0)[0], 0.0) and TOLERANCE.is_close(pc2.get_normal(0)[1], 1.0) and TOLERANCE.is_close(pc2.get_normal(0)[2], 0.0))
+    MINI_CHECK(TOLERANCE.is_close(pc_iadd.get_point(0)[0], 11.0) and TOLERANCE.is_close(pc_iadd.get_point(0)[1], 22.0) and TOLERANCE.is_close(pc_iadd.get_point(0)[2], 33.0))
+    MINI_CHECK(TOLERANCE.is_close(pc_isub.get_point(0)[0], -9.0) and TOLERANCE.is_close(pc_isub.get_point(0)[1], -18.0) and TOLERANCE.is_close(pc_isub.get_point(0)[2], -27.0))
+    MINI_CHECK(TOLERANCE.is_close(pc_add.get_point(0)[0], 11.0) and TOLERANCE.is_close(pc_add.get_point(0)[1], 22.0) and TOLERANCE.is_close(pc_add.get_point(0)[2], 33.0))
+    MINI_CHECK(TOLERANCE.is_close(pc_sub.get_point(0)[0], -9.0) and TOLERANCE.is_close(pc_sub.get_point(0)[1], -18.0) and TOLERANCE.is_close(pc_sub.get_point(0)[2], -27.0))
+    MINI_CHECK(TOLERANCE.is_close(pc3.get_point(0)[0], 1.0) and TOLERANCE.is_close(pc3.get_point(0)[1], 2.0) and TOLERANCE.is_close(pc3.get_point(0)[2], 3.0))
+    MINI_CHECK(len(pc4) == 3 and pc4.color_count() == 3 and pc4.normal_count() == 3)
+    MINI_CHECK(TOLERANCE.is_close(pc4.get_point(1)[0], 1.0) and TOLERANCE.is_close(pc4.get_point(1)[1], 0.0) and TOLERANCE.is_close(pc4.get_point(1)[2], 0.0))
+    MINI_CHECK(pc4.get_color(1)[0] == 0 and pc4.get_color(1)[1] == 255 and pc4.get_color(1)[2] == 0 and pc4.get_color(1)[3] == 255)
+    MINI_CHECK(TOLERANCE.is_close(pc4.get_normal(1)[0], 0.0) and TOLERANCE.is_close(pc4.get_normal(1)[1], 0.0) and TOLERANCE.is_close(pc4.get_normal(1)[2], 1.0))
 
 
 @MINI_TEST("PointCloud", "transform")
@@ -158,13 +105,9 @@ def test_pointcloud_transform():
     pc2.xform = Xform.translation(10.0, 20.0, 30.0)
     pc3 = pc2.transformed()
 
-    pt = pc.get_point(0)
-    pt3 = pc3.get_point(0)
-    pt2 = pc2.get_point(0)
-
-    MINI_CHECK(TOLERANCE.is_close(pt[0], 11.0) and TOLERANCE.is_close(pt[1], 22.0) and TOLERANCE.is_close(pt[2], 33.0))
-    MINI_CHECK(TOLERANCE.is_close(pt3[0], 11.0) and TOLERANCE.is_close(pt3[1], 22.0) and TOLERANCE.is_close(pt3[2], 33.0))
-    MINI_CHECK(TOLERANCE.is_close(pt2[0], 1.0) and TOLERANCE.is_close(pt2[1], 2.0) and TOLERANCE.is_close(pt2[2], 3.0))
+    MINI_CHECK(TOLERANCE.is_close(pc.get_point(0)[0], 11.0) and TOLERANCE.is_close(pc.get_point(0)[1], 22.0) and TOLERANCE.is_close(pc.get_point(0)[2], 33.0))
+    MINI_CHECK(TOLERANCE.is_close(pc3.get_point(0)[0], 11.0) and TOLERANCE.is_close(pc3.get_point(0)[1], 22.0) and TOLERANCE.is_close(pc3.get_point(0)[2], 33.0))
+    MINI_CHECK(TOLERANCE.is_close(pc2.get_point(0)[0], 1.0) and TOLERANCE.is_close(pc2.get_point(0)[1], 2.0) and TOLERANCE.is_close(pc2.get_point(0)[2], 3.0))
 
 
 @MINI_TEST("PointCloud", "json_roundtrip")
@@ -186,15 +129,11 @@ def test_pointcloud_json_roundtrip():
     pc.json_dump(fname)
     loaded = PointCloud.json_load(fname)
 
-    pt0 = loaded.get_point(0)
-    col0 = loaded.get_color(0)
-    norm0 = loaded.get_normal(0)
-
     MINI_CHECK(loaded.name == "test_pointcloud")
     MINI_CHECK(len(loaded) == 2)
-    MINI_CHECK(TOLERANCE.is_close(pt0[0], 1.0) and TOLERANCE.is_close(pt0[1], 2.0) and TOLERANCE.is_close(pt0[2], 3.0))
-    MINI_CHECK(col0[0] == 255 and col0[1] == 0 and col0[2] == 0)
-    MINI_CHECK(TOLERANCE.is_close(norm0[2], 1.0))
+    MINI_CHECK(TOLERANCE.is_close(loaded.get_point(0)[0], 1.0) and TOLERANCE.is_close(loaded.get_point(0)[1], 2.0) and TOLERANCE.is_close(loaded.get_point(0)[2], 3.0))
+    MINI_CHECK(loaded.get_color(0)[0] == 255 and loaded.get_color(0)[1] == 0 and loaded.get_color(0)[2] == 0)
+    MINI_CHECK(TOLERANCE.is_close(loaded.get_normal(0)[2], 1.0))
 
 
 @MINI_TEST("PointCloud", "protobuf_roundtrip")
@@ -216,15 +155,11 @@ def test_pointcloud_protobuf_roundtrip():
     pc.protobuf_dump(fname)
     loaded = PointCloud.protobuf_load(fname)
 
-    pt0 = loaded.get_point(0)
-    col0 = loaded.get_color(0)
-    norm0 = loaded.get_normal(0)
-
     MINI_CHECK(loaded.name == "test_pointcloud")
     MINI_CHECK(len(loaded) == 2)
-    MINI_CHECK(TOLERANCE.is_close(pt0[0], 1.0) and TOLERANCE.is_close(pt0[1], 2.0) and TOLERANCE.is_close(pt0[2], 3.0))
-    MINI_CHECK(col0[0] == 255 and col0[1] == 0 and col0[2] == 0)
-    MINI_CHECK(TOLERANCE.is_close(norm0[2], 1.0))
+    MINI_CHECK(TOLERANCE.is_close(loaded.get_point(0)[0], 1.0) and TOLERANCE.is_close(loaded.get_point(0)[1], 2.0) and TOLERANCE.is_close(loaded.get_point(0)[2], 3.0))
+    MINI_CHECK(loaded.get_color(0)[0] == 255 and loaded.get_color(0)[1] == 0 and loaded.get_color(0)[2] == 0)
+    MINI_CHECK(TOLERANCE.is_close(loaded.get_normal(0)[2], 1.0))
 
 
 if __name__ == "__main__":
