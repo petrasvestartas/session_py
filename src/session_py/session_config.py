e904f4a77a016f94bca204aa4cddaa0d129a5b40
Petras Vestartas <petrasvestartas@gmail.com>
2026-03-30 18:37:58 +0200

    session scale

--- src/session_py/session_config.py
+++ src/session_py/session_config.py
@@ -5,13 +5,15 @@ class SessionConfig:
         if not cls._instance:
             cls._instance = object.__new__(cls)
             cls._instance.explode_mesh_faces = False
+            cls._instance.scale_factor = 1.0
         return cls._instance
 
     def reset(self):
         self.explode_mesh_faces = False
+        self.scale_factor = 1.0
 
     def __repr__(self):
-        return f"SessionConfig(explode_mesh_faces={self.explode_mesh_faces})"
+        return f"SessionConfig(explode_mesh_faces={self.explode_mesh_faces}, scale_factor={self.scale_factor})"
 
 
 SESSION_CONFIG = SessionConfig()
--- src/session_py/session_config_test.py
+++ src/session_py/session_config_test.py
@@ -7,6 +7,7 @@ from session_py.session_config import SESSION_CONFIG, SessionConfig
 @MINI_TEST("SessionConfig", "Default Values")
 def test_session_config_default_values():
     MINI_CHECK(not SESSION_CONFIG.explode_mesh_faces)
+    MINI_CHECK(SESSION_CONFIG.scale_factor == 1.0)
 
 
 @MINI_TEST("SessionConfig", "Runtime Modification")
@@ -14,8 +15,12 @@ def test_session_config_runtime_modification():
     MINI_CHECK(not SESSION_CONFIG.explode_mesh_faces)
     SESSION_CONFIG.explode_mesh_faces = True
     MINI_CHECK(SESSION_CONFIG.explode_mesh_faces)
+    MINI_CHECK(SESSION_CONFIG.scale_factor == 1.0)
+    SESSION_CONFIG.scale_factor = 0.001
+    MINI_CHECK(SESSION_CONFIG.scale_factor == 0.001)
     SESSION_CONFIG.reset()
     MINI_CHECK(not SESSION_CONFIG.explode_mesh_faces)
+    MINI_CHECK(SESSION_CONFIG.scale_factor == 1.0)
 
 
 if __name__ == "__main__":
